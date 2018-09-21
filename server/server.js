c90ae88470249b2b55f66852bdabc8d68540fa3b
marvpaul <marvin.krueger@yahoo.de>
2018-09-21 23:09:09 +0200

    Changed header

--- server/server.js
+++ server/server.js
@@ -4,15 +4,12 @@ var bodyParser = require('body-parser');
 var mongo = require('mongoose'); 
 
 let connectionString = "";
-let frontendIp = "";
 if(process.env.connectionString == null || process.env.connectionString == ""){
     var fs = require("fs");
     var config = JSON.parse(fs.readFileSync("config.json"));
     connectionString = config.connectionString; 
-    frontendIp = config.frontendIp; 
 } else{
     connectionString = process.env.connectionString;
-    frontendIp = process.env.frontendIp;
 }
 
 
@@ -28,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin: *'); 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
     res.setHeader('Access-Control-Allow-Credentials',  true);
