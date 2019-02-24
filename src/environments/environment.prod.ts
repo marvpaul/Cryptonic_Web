dbcf3a029608f33cd46985797802dbb7af4a711d
marvpaul <marvin.krueger@yahoo.de>
2019-02-24 18:49:17 +0100

    Added enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'https://dry-lake-13557.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiEndpoint: 'https://dry-lake-13557.herokuapp.com/'
 };
 
 /*
